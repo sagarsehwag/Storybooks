a4475dbb99b8c2163eb03c192dd870a36ef9e804
Sagar Sehwag <isagarsehwag@gmail.com>
2019-01-27 01:07:34 +0530

    Deployment Ready

---
+++ config/key_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+};
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://sagarsehwag:mlab0026@ds011745.mlab.com:11745/storybooks-dev",
-  googleClientID: "415497054607-fdf51hbf8ibi09296s7qfobk28us7sp8.apps.googleusercontent.com",
-  googleClientSecret: "FwrrIEE9HWWUI1vbUrFXFYos"
-};
+if (process.env.NODE_ENV === "production") {
+    module.exports = require("./key_prod");
+} else {
+    module.exports = require("./keys_dev");
+}
